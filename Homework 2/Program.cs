508c452e3b4977523c7c88650ec65f49162c2517
Lenovo <Lenovo@DESKTOP-AJ5INH1>
2022-07-18 21:20:57 +0300

    Добавьте файлы проекта.

---
+++ Homework 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using static System.Console;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            OutputEncoding = Encoding.Unicode;
+            Write("Write nomber - ");
+            int num = Convert.ToInt32(ReadLine());
+            Write("Write procent - ");
+            int pro = Convert.ToInt32(ReadLine());
+            int rez;
+            rez = num / 100 * pro;
+            Console.WriteLine(+pro + "widsotok wid chusla " + num + " = " + rez);
+        }
+    }
+}
